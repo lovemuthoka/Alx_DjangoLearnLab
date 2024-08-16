a28f1f635e8f70bd8dc0bf523a3feb905089b50c
lovemuthoka <lovemuthoka3@gmail.com>
2024-08-16 21:52:00 +0300

    details

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import views as auth_views
- 
+from .views import admin_view, librarian_view, member_view
 urlpatterns = [
     # URL pattern for the function-based view that lists all books
     path('books/', book_list_view, name='book-list'),
@@ -20,4 +20,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     
+]
+
+urlpatterns = [
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+    # Other URL patterns...
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-  
+from django.contrib.auth.decorators import user_passes_test
 
 # Function-based view to list all books
 def book_list_view(request):
@@ -35,3 +35,24 @@ class RegisterView(CreateView):
     form_class= UserCreationForm()
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
