fb711afb161a2bc0ae2fc72f77e78a05cfac50f0
lovemuthoka <lovemuthoka3@gmail.com>
2024-08-16 20:58:28 +0300

    details

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from .views import list_books
+from .views import CustomLoginView, CustomLogoutView, RegisterView
 
+ 
 urlpatterns = [
     # URL pattern for the function-based view that lists all books
     path('books/', book_list_view, name='book-list'),
 
     # URL pattern for the class-based view that displays details for a specific library
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+
+    path('login/', CustomLoginView.as_view(), name='login'),
+     path('logout/', CustomLogoutView.as_view(), name='logout'),
+     path('register/', RegisterView.as_view(), name='register'),
+
 ]
