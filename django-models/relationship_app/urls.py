402ed9aff880669c148007be74b1bdf35be73a1c
lovemuthoka <lovemuthoka3@gmail.com>
2024-08-18 10:12:42 +0300

    details

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -33,7 +33,8 @@ urlpatterns = [
 
 
 urlpatterns = [
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+
 ]
