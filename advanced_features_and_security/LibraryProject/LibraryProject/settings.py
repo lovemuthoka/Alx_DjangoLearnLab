c95ed22fba4a26fc264cf2a98e0d8b4e190e50e1
lovemuthoka <lovemuthoka3@gmail.com>
2024-08-20 18:32:45 +0300

    details

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add this to use the custom user model
+AUTH_USER_MODEL = 'your_app_name.CustomUser'
--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from .models import CustomUser
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
@@ -62,3 +65,33 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
 
     def __str__(self):
         return self.title
+    
+    class CustomUserManager(models.Manager):
+
+      def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError('Users must have a username')
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        class SomeModel(models.Model):
+            user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
