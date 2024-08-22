a0bf1987777663e7a04af287efe69ae129b585cb
lovemuthoka <lovemuthoka3@gmail.com>
2024-08-22 03:55:48 +0300

    details

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.contrib.auth.models import AbstractUser,BaseUserManager
+from .models import Book
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
@@ -64,3 +65,59 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     def __str__(self):
         return self.title
     
+    # class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     publication_year = models.IntegerField
+
+#     def __str__(self):
+#         return self.title
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=False)
+    profile_photo = models.ImageField(blank=False)
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("user must have email")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+        
+
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password) 
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+    # admin.site.register(Book)
+
+# # form class admin 
+# class BookAdmin(admin.ModelAdmin):
+# # create list display
+#     list_display = ('title', 'author', 'publication_year')
+
+# #create list filter 
+#     list_filter = ('author', 'publication_year')
+# # create search field 
+#     search_fields = ('title', 'author')
+
+# # register  Book and Bookadmon
+# admin.site.register(Book, BookAdmin)
+
+# # Register your models here.
+# # Register your models here.
+
+from django.contrib.auth.admin import UserAdmin as CustomUserAdmin
+from .models import CustomUser
+
+
+class UserAdmin(CustomUserAdmin):
+    pass
+
+admin.site.register(CustomUser, CustomUserAdmin)
