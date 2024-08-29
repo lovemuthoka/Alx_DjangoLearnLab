dfc9fef1c3525c5ecbb4100803ae8628cad10fb4
lovemuthoka <lovemuthoka3@gmail.com>
2024-08-29 17:08:58 +0300

    api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -10,5 +10,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api.urls/', include('api.urls')),
 ]
