1261735b705a39a3e7d1ef60d68d1d68dd2cb2d3
lovemuthoka <lovemuthoka3@gmail.com>
2024-09-07 23:20:10 +0300

    booklist

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -10,6 +10,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 # List all books (read-only access for unauthenticated users)
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
