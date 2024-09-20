6e4a29e7a9fd7ffcf0b6264b9355be63e096da55
lovemuthoka <lovemuthoka3@gmail.com>
2024-09-20 14:44:46 +0300

    accounts

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -15,3 +15,15 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['post', 'user']
+    
